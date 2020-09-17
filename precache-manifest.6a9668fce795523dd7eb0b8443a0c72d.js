75819590323384bfbbed6222b928c819d4e6ab53
Stas Mikhailov <mik.stanis@yandex.ru>
2020-09-17 18:31:30 +0300

    Updates

---
+++ precache-manifest.6a9668fce795523dd7eb0b8443a0c72d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9785dacda5471333ca04fd99fc3fdf17",
+    "url": "./index.html"
+  },
+  {
+    "revision": "894ccfde0b82006d7de6",
+    "url": "./static/css/main.3ebd268f.chunk.css"
+  },
+  {
+    "revision": "55adae06a38a99f88277",
+    "url": "./static/js/2.4c83c19a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4c83c19a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "894ccfde0b82006d7de6",
+    "url": "./static/js/main.b83c7b9e.chunk.js"
+  },
+  {
+    "revision": "40be5c299465cd069b37",
+    "url": "./static/js/runtime-main.6016a75d.js"
+  },
+  {
+    "revision": "1aad4fbefa7ae31f68e62f536dcfc511",
+    "url": "./static/media/NotoSans-Bold.1aad4fbe.woff2"
+  },
+  {
+    "revision": "292d88d9847105f153a06ae4edeef240",
+    "url": "./static/media/NotoSans-Bold.292d88d9.eot"
+  },
+  {
+    "revision": "47a1f4ea454b5606253d13e20286cfd5",
+    "url": "./static/media/NotoSans-Bold.47a1f4ea.woff"
+  },
+  {
+    "revision": "80ddcc3a5cc00897d2a2af55b99436b6",
+    "url": "./static/media/NotoSans-Bold.80ddcc3a.ttf"
+  },
+  {
+    "revision": "1da01dfedf003e904c8108a7a4bc7abd",
+    "url": "./static/media/NotoSans-BoldItalic.1da01dfe.woff2"
+  },
+  {
+    "revision": "61c1c3449ce3b73b5309d694c7d548a9",
+    "url": "./static/media/NotoSans-BoldItalic.61c1c344.eot"
+  },
+  {
+    "revision": "986d42505729752da2cd1bcf6cf749a3",
+    "url": "./static/media/NotoSans-BoldItalic.986d4250.woff"
+  },
+  {
+    "revision": "b16c5bc8aeb86c517b3e50947f7a44ee",
+    "url": "./static/media/NotoSans-BoldItalic.b16c5bc8.ttf"
+  },
+  {
+    "revision": "63bdb17a6cc3809cbb5f0ec2faf3837f",
+    "url": "./static/media/NotoSans-Italic.63bdb17a.woff2"
+  },
+  {
+    "revision": "90e89067a263d7d6c8fc53914f128591",
+    "url": "./static/media/NotoSans-Italic.90e89067.eot"
+  },
+  {
+    "revision": "9d49ec9bf8e5aca62109812bc6b2e4fb",
+    "url": "./static/media/NotoSans-Italic.9d49ec9b.ttf"
+  },
+  {
+    "revision": "d10f294661b80b638f86a8e9526e21ee",
+    "url": "./static/media/NotoSans-Italic.d10f2946.woff"
+  },
+  {
+    "revision": "1c139b0d4abd4fb5bdf6c887625d0fdd",
+    "url": "./static/media/NotoSans.1c139b0d.ttf"
+  },
+  {
+    "revision": "59f950211c3446db9947f8c3091cab64",
+    "url": "./static/media/NotoSans.59f95021.eot"
+  },
+  {
+    "revision": "a9b69cb2331f66472534819307eec9aa",
+    "url": "./static/media/NotoSans.a9b69cb2.woff"
+  },
+  {
+    "revision": "f9d915cf691634faab753a5c7c35a5ac",
+    "url": "./static/media/NotoSans.f9d915cf.woff2"
+  }
+]);
